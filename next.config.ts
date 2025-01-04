9a8a603e039e4b76e1d0d2d815e8a3ffcc58b7dc
Adrien Cohen <127618020+adrienckr@users.noreply.github.com>
2025-01-03 18:11:33 -0800

    added openai key to next config js

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 import vercelConfig from "./vercel.json" assert { type: "json" };
 
 const nextConfig: NextConfig = {
-  env: vercelConfig.env,
+  env: {
+    ...vercelConfig.env,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
 };
 
 export default nextConfig;
